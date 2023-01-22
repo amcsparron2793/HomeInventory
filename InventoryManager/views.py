6468958ee34ab158403ce39d777cf6ffc1404ada
amcsparron2793 <69968398+amcsparron2793@users.noreply.github.com>
2023-01-22 13:33:48 -0500

    added basic test view/index template and urls

--- InventoryManager/views.py
+++ InventoryManager/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import ItemNames
+
 
 # Create your views here.
+def index(request):
+    items_by_name = ItemNames.objects.all()
+    context = {'items': items_by_name}
+    return render(request, 'InventoryManager/index.html', context)
