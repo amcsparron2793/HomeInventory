02dee51df9859cb0ceb192b434b21392b6303a3b
amcsparron2793 <69968398+amcsparron2793@users.noreply.github.com>
2023-01-22 16:47:02 -0500

    added rudimentary form processing to index.html

--- InventoryManager/views.py
+++ InventoryManager/views.py
@@ -7,3 +7,12 @@ def index(request):
     #items_by_name = ItemNames.objects.all()
     context = {'in_out_form': InOutForm}
     return render(request, 'InventoryManager/index.html', context)
+
+
+def lookup(request):
+    if request.method == "POST":
+        context = {'in_out_form': InOutForm}
+        Form = InOutForm(request.POST)
+        if Form.is_valid():
+            context['InfoSubmitted'] = Form.data
+            return render(request, 'InventoryManager/index.html', context)
