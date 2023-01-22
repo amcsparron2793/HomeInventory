3397490581c2d4df072e3b1bc27b304c54855f18
amcsparron2793 <69968398+amcsparron2793@users.noreply.github.com>
2023-01-22 15:58:23 -0500

    changed context key to 'in_out_form' in views.py and index.html

--- InventoryManager/views.py
+++ InventoryManager/views.py
@@ -5,5 +5,5 @@ from .forms import InOutForm
 # Create your views here.
 def index(request):
     #items_by_name = ItemNames.objects.all()
-    context = {'lookup_form': InOutForm}
+    context = {'in_out_form': InOutForm}
     return render(request, 'InventoryManager/index.html', context)
