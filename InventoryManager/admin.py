00f0497786fa1fbd4b276caa2eaa9fc66b188186
amcsparron2793 <69968398+amcsparron2793@users.noreply.github.com>
2023-01-22 12:20:29 -0500

    added verbose_name_plurals to meta of all models. registered all models with admin.py

--- InventoryManager/admin.py
+++ InventoryManager/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import ItemNames, Barcodes, TechCategories, TechInventory, Manufacturers, InOutStatus, UseDescription
 
 # Register your models here.
+admin.site.register(ItemNames)
+admin.site.register(Barcodes)
+admin.site.register(TechCategories)
+admin.site.register(TechInventory)
+admin.site.register(Manufacturers)
+admin.site.register(InOutStatus)
+admin.site.register(UseDescription)
--- InventoryManager/models.py
+++ InventoryManager/models.py
@@ -5,13 +5,22 @@ from django.db import models
 class ItemNames(models.Model):
     item_name = models.CharField(max_length=250, unique=True)
 
+    class Meta:
+        verbose_name_plural = "Item Names"
+
 
 class TechCategories(models.Model):
     category_name = models.CharField(max_length=250, unique=True)
 
+    class Meta:
+        verbose_name_plural = "Tech Categories"
+
 
 class Manufacturers(models.Model):
     manufacturer_name = models.CharField(max_length=250, unique=True)
+
+    class Meta:
+        verbose_name_plural = "Manufacturers"
 
 
 class TechInventory(models.Model):
@@ -21,15 +30,24 @@ class TechInventory(models.Model):
     item_name = models.ForeignKey(to=ItemNames, on_delete=models.DO_NOTHING)
     manufacturer = models.ForeignKey(to=Manufacturers, on_delete=models.DO_NOTHING)
 
+    class Meta:
+        verbose_name_plural = "Tech Inventory"
+
 
 class Barcodes(models.Model):
     tech_inventory = models.ForeignKey(to=TechInventory, on_delete=models.DO_NOTHING)
     upc = models.IntegerField(unique=True)
 
+    class Meta:
+        verbose_name_plural = "Barcodes"
+
 
 class InOutStatus(models.Model):
     checked_in = models.BooleanField(default=True)
     barcode = models.ForeignKey(to=Barcodes, on_delete=models.DO_NOTHING)
+
+    class Meta:
+        verbose_name_plural = "InOutStatus"
 
 
 class UseDescription(models.Model):
