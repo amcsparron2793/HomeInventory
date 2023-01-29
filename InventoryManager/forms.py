165e2c15420fdd1b2f26b21ae5d9242c294df6e1
amcsparron2793 <69968398+amcsparron2793@users.noreply.github.com>
2023-01-29 09:56:35 -0500

    added a upc num validator to InOutForm and changed the success message to white

--- InventoryManager/forms.py
+++ InventoryManager/forms.py
@@ -1,15 +1,22 @@
 from django import forms
 from django.core.exceptions import ValidationError
+
 FormChoices = ["Status Check", "Check In", "Check Out"]
 
 
 class InOutForm(forms.Form):
-    def _UPC_Validator(self):
+    def _UPCLengthValidator(self):
         if len(str(self)) == 9:
             return True
         else:
             raise ValidationError("UPC must be 9 digits")
 
-    UPC = forms.IntegerField(validators=[_UPC_Validator])
+    def _UPCNumValidator(self):
+        if str(self).isnumeric():
+            return True
+        else:
+            raise ValidationError("UPC cannot have letters or special characters")
+
+    UPC = forms.IntegerField(validators=[_UPCLengthValidator, _UPCNumValidator])
     Scan_Type = forms.ChoiceField(choices=[(x, x) for x in FormChoices])
 
